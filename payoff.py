9ecc5248940a20ceebe38abe4fe0733ebcee0c04
Jacob Lewallen <jlewallen@gmail.com>
2021-11-21 13:56:58 -0800

    feat(payoff): Wire up all the command line args.

--- payoff.py
+++ payoff.py
@@ -11,27 +11,27 @@ from dataclasses import dataclass, field
 @dataclass
 class PayoffCalculator:
     total: float
-    name: str
     date: datetime.date
     payments: int
+    source: str
+    destination: str
+    payee: str
 
     def calculate(self):
         indention = "    "
-        source = "[allocations:checking:shaved]"
-        destination = "[allocations:checking:savings:main:payoff]"
         remaining = int(self.total * 100)
         payment = int(remaining / self.payments)
 
         log.info(
-            f"generating {self.total} payoff {source} -> {destination} for '{self.name}'"
+            f"generating {self.total} payoff {self.source} -> {self.destination} for '{self.payee}'"
         )
 
         payment_date = self.date
         for n in range(self.payments):
             p = remaining if n == self.payments - 1 else payment
-            print(f"{payment_date.strftime('%Y/%m/%d')} * {self.name}")
-            print(f"{indention}{source:50}-${p/100:.2f}")
-            print(f"{indention}{destination:50} ${p/100:.2f}")
+            print(f"{payment_date.strftime('%Y/%m/%d')} * {self.payee}")
+            print(f"{indention}{self.source:50}-${p/100:.2f}")
+            print(f"{indention}{self.destination:50} ${p/100:.2f}")
             print()
             remaining -= p
             payment_date += relativedelta.relativedelta(months=1)
@@ -49,16 +49,19 @@ if __name__ == "__main__":
     log = logging.getLogger("payoff")
 
     parser = argparse.ArgumentParser(description="payoff tool")
-    parser.add_argument("-d", "--debug", action="store_true", default=False)
-    parser.add_argument("-n", "--name", action="store", default="some random expense")
+    parser.add_argument("-D", "--debug", action="store_true", default=False)
+    parser.add_argument("-p", "--payee", action="store", default="some random expense")
     parser.add_argument(
-        "-e", "--expense", action="store", default="some random expense"
+        "-s", "--source", action="store", default="[allocations:checking:auto:payoff]"
     )
     parser.add_argument(
-        "-a", "--allocation", action="store", default="some random expense"
+        "-d",
+        "--destination",
+        action="store",
+        default="[allocations:checking:savings:main:payoff]",
     )
     parser.add_argument("-t", "--total", action="store", default=1000, type=float)
-    parser.add_argument("-p", "--payments", action="store", default=12, type=int)
+    parser.add_argument("-n", "--payments", action="store", default=12, type=int)
     args = parser.parse_args()
 
     if args.debug:
@@ -66,6 +69,11 @@ if __name__ == "__main__":
 
     date = datetime.date.today()
     calculator = PayoffCalculator(
-        name=args.name, total=args.total, payments=args.payments, date=date
+        total=args.total,
+        payments=args.payments,
+        date=date,
+        source=args.source,
+        destination=args.destination,
+        payee=args.payee,
     )
     calculator.calculate()
